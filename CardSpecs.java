5dd97c2e62e9e6127b88b7f24431552a2246053e
Alexey-B0 <alexey.budnyev@ucdconnect.ie>
2023-02-01 18:48:44 +0000

    added class that can generate random attributes to tiles

---
+++ CardSpecs.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+enum habitat {
+    WOODLAND,
+    WATERAREA,
+    SANDY,
+    MOUNTAINS
+}
+
+enum wildlife {
+    FOX,
+    ELK,
+    BEAR,
+    HAWK,
+    SALMON
+}
+
+public class CreateTile {
+
+    Random place = new Random();
+
+    public String chooseHabitat() {
+
+        int x = place.nextInt(4);
+
+        return String.valueOf(habitat.values()[x]);
+    }
+
+    public String chooseWildlife() {
+
+        int y = place.nextInt(5);
+
+        return String.valueOf(wildlife.values()[y]);
+    }
+}
