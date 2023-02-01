f7bd8f298d0323ba939e9372da9f90b94303f7c7
lukek2202 <lgking2112@gmail.com>
2023-02-01 13:38:53 +0000

    Added colours class for text colour

---
+++ ViewColours.java
@@ -0,0 +1,27 @@
+public class ViewColours {
+
+    public static final String RESET = "\033[0m";  // Text Reset
+    public static final String BLACK = "\033[38;5;0m";   // BLACK
+    public static final String RED = "\033[38;5;1m";     // RED
+    public static final String GREEN = "\033[38;5;70m";   // GREEN
+    public static final String D_GREEN = "\033[38;5;22m";   // DARK GREEN
+    public static final String YELLOW = "\033[38;5;11m";  // YELLOW
+    public static final String BLUE = "\033[38;5;26m";    // BLUE
+    public static final String PURPLE = "\033[38;5;5m";  // PURPLE
+    public static final String CYAN = "\033[38;5;6m";    // CYAN
+    public static final String WHITE = "\033[38;5;15m";   // WHITE
+    public static final String GREY = "\033[38;5;8m"; // GREY
+
+
+
+    public static final String BLACK_BG = "\033[48;5;0m";   // BLACK
+    public static final String RED_BG = "\033[48;5;1m";     // RED
+    public static final String GREEN_BG = "\033[48;5;70m";   // GREEN
+    public static final String D_GREEN_BG = "\033[48;5;22m";   // DARK GREEN
+    public static final String YELLOW_BG = "\033[48;5;11m";  // YELLOW
+    public static final String BLUE_BG = "\033[48;5;26m";    // BLUE
+    public static final String PURPLE_BG = "\033[48;5;5m";  // PURPLE
+    public static final String CYAN_BG = "\033[48;5;6m";    // CYAN
+    public static final String WHITE_BG = "\033[48;5;15m";   // WHITE
+    public static final String GREY_BG = "\033[48;5;8m"; // GREY
+}
