ac03f095888ebecd9bbb2ffe499f36cbb49452ff
Alexey-B0 <alexey.budnyev@ucdconnect.ie>
2023-02-01 21:47:29 +0000

    deck didn't add

--- Deck.java
+++ Deck.java
@@ -1,2 +1,22 @@
+import java.util.Random;
+
 public class Deck {
+
+
+    Random place = new Random();
+
+    public String chooseHabitat() {
+
+        int x = place.nextInt(4);
+
+        return String.valueOf(habitat.values()[x]);
+    }
+
+    public String chooseWildlife() {
+
+        int y = place.nextInt(5);
+
+        return String.valueOf(wildlife.values()[y]);
+    }
+}
 }
