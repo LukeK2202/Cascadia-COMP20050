3f96285f1508506981e2395c73f8449ce3abcd14
lukek2202 <lgking2112@gmail.com>
2023-02-02 13:36:19 +0000

    Updated the colours class

--- ViewColours.java
+++ ViewColours.java
@@ -11,6 +11,9 @@ public class ViewColours {
     public static final String CYAN = "\033[38;5;6m";    // CYAN
     public static final String WHITE = "\033[38;5;15m";   // WHITE
     public static final String GREY = "\033[38;5;8m"; // GREY
+    public static final String ORANGE = "\033[38;5;214m"; //ORANGE
+    public static final String BROWN = "\033[38;5;94m"; //BROWN
+    public static final String PINK = "\033[38;5;207m"; //PINK
 
 
 
@@ -24,4 +27,7 @@ public class ViewColours {
     public static final String CYAN_BG = "\033[48;5;6m";    // CYAN
     public static final String WHITE_BG = "\033[48;5;15m";   // WHITE
     public static final String GREY_BG = "\033[48;5;8m"; // GREY
+    public static final String ORANGE_BG = "\033[48;5;214m"; //ORANGE
+    public static final String BROWN_BG = "\033[48;5;94m"; //BROWN
+    public static final String PINK_BG = "\033[48;5;207m"; //PINK
 }
