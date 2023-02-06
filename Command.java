4815eab26d3b70ac25a55119762700984de5e288
DavidKenny3 <david.kenny3@ucdconnect.ie>
2023-02-06 12:04:32 +0000

    next and quit

--- Command.java
+++ Command.java
@@ -1,10 +1,46 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Command {
+public class Command{
     //Main Control Class, Purpose to call commands.
 
     //Array list to contain all named users and the play order in which they are played
-    ArrayList<String> Players = new ArrayList<String>();
+    static ArrayList<String> Players = new ArrayList<String>();
+
+    private static int currentPlayerPosition = 0;
+
+    public static int numPlayers;
+
+    private static boolean gameInProgress = true;
+
+    private static String getCurrPlayer(){
+        return Players.get(currentPlayerPosition);
+    }
+    private static void nextPlayer(){
+        currentPlayerPosition = (currentPlayerPosition + 1) % Players.size();
+    }
+
+    public static void quitFeature(){
+        Scanner in = new Scanner(System.in);
+
+        while (gameInProgress){
+            System.out.println("enter q to quit");
+            String input = in.nextLine();
+
+            if(input.equals("q")){
+                View.showQuit();
+            }
+            else{
+                System.out.println("continuing game");
+            }
+        }
+        in.close();
+    }
+
+    private static void quitGame() {
+        View.showQuit();
+        gameInProgress = false;
+    }
 
     //Command method, recieves input, formats and sets the command settings to the received command,
     //to be used in Casc class
@@ -17,4 +53,5 @@ public class Command {
     public static boolean isValid(String input) {
         return true;
     }
+    
 }
