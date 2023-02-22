b8ae5afa04fa9c176c3bebd24bfdec7a5a0eed0e
lukek2202 <lgking2112@gmail.com>
2023-02-22 13:04:55 +0000

    updated tile to allow different variations

--- Board.java
+++ Board.java
@@ -1,20 +1,31 @@
+import java.util.ArrayList;
+
+//Main board class that will contain the tiles for each user
 public class Board {
 
+    //Main board matrix, a grid of tile classes
     Tile[][] board = new Tile[11][11];
+    ArrayList<int[]> occPlaces = new ArrayList<int[]>();
+    //Board constructor, adds 3 tiles as a started tile. 1 keystone and 2 randoms
+    //Origin of board is 6,6.
     Board() {
-        addTile(new Tile(Habitat.chooseHabitatArr(), Wildlife.chooseWildlifeArr()), 6, 6);
-        addTile(new Tile(Habitat.chooseHabitatArr(), Wildlife.chooseWildlifeArr()), 7, 5);
-        addTile(new Tile(Habitat.chooseHabitatArr(), Wildlife.chooseWildlifeArr()), 7, 6);
+
+        addTile(new Tile(Tile.tileTypes.KEYSTONE), 6, 6);
+        addTile(new Tile(Tile.tileTypes.RANDOM), 7, 5);
+        addTile(new Tile(Tile.tileTypes.RANDOM), 7, 6);
     }
 
+    //Returns board length
     public int getBoardLength() {
         return board.length;
     }
 
+    //Returns board width
     public int getBoardWidth() {
         return board[0].length;
     }
 
+    //Method to check if a row contains any non initiated tiles
     public boolean isRowNull(int row) {
         for(int i = 0; i < board[row].length; i++) {
             if(board[row][i] == null) {
@@ -24,12 +35,14 @@ public class Board {
         return false;
     }
 
+    //Method to populate an entire row with blank tiles
     private void populateRow(int row) {
         for(int i = 0; i < board[row].length; i++) {
-            board[row][i] = new Tile();
+            board[row][i] = new Tile(Tile.tileTypes.BLANK);
         }
     }
 
+    //Method to add a tile to the board, requiring the row and column
     public void addTile(Tile tile ,int row, int column) {
         if(isRowNull(row)) {
             populateRow(row);
--- Tile.java
+++ Tile.java
@@ -1,47 +1,146 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
+//Tile class to construct and use tiles for the game
 public class Tile {
 
-
-    Boolean blank = false;
-    ArrayList<Habitat> habitats = new ArrayList<>();
-    ArrayList<Wildlife> wildlife = new ArrayList<>();
-    private String[][] tile = new String[4][4];;
-    public static String space = "   ";
-    public static String blankSpace = ViewColours.WHITE_BG + space + ViewColours.RESET;
-
-    String[] top = {tile[0][0], tile[0][1], tile[0][2], tile[0][3]};
-    Tile() {
-        for(int i = 0; i < tile.length; i++) {
-            for(int j = 0; j < tile[i].length; j++) {
-                tile[i][j] = space;
-            }
-        }
-        this.blank = true;
+    //Enum used to determine what tile type is wanted
+    public static enum tileTypes {
+        KEYSTONE,
+        RANDOM,
+        BLANK
     }
+    //Boolean to show if tile is a blank tile or not
+    Boolean blank = false;
+    //Array list to be populated with the habitats the tile has
+    ArrayList<Habitat> habitats = new ArrayList<>();
+    //Array list to be populated with the wildlife the tile has
+    ArrayList<Wildlife> wildlife = new ArrayList<>();
+    //Main tile array, a 4x4 grid of strings which become the tile
+    private String[][] tile = new String[4][4];
+    //Spacing used as a constant between tiles
+    public static String space = "   ";
+    //Blank space(white)
+    public static String whiteSpace = ViewColours.WHITE_BG + space + ViewColours.RESET;
 
-    Tile(ArrayList<Habitat> habitat, ArrayList<Wildlife> animal) {
-        habitats = new ArrayList<>(habitat);
-        wildlife = new ArrayList<>(animal);
+    //Main tile constructor
+    Tile(tileTypes type) {
+        //Keystone tile constructor
+        if(type == tileTypes.KEYSTONE) {
+            //Populates the habitats and wildlife arraylists
+            habitats = new ArrayList<Habitat>();
+            habitats.add(Habitat.chooseHabitat());
+            wildlife = new ArrayList<Wildlife>();
+            wildlife.add(Wildlife.chooseWildlife());
 
-        for(int i = 0; i < tile.length; i++) {
-            for(int j = 0; j < tile.length; j++) {
-                if(i == 0 || i == tile.length - 1 || j == 0 || j == tile[i].length - 1) {
-                    if((i == 0 || (i < tile.length - 1 && j == 0)) && habitat.size() > 1) {
-                        this.tile[i][j] = habitat.get(1) + space + ViewColours.RESET;
+            //Double for loop to iterate through each entry in the 4x4 array
+            for(int i = 0; i < tile.length; i++) {
+                for (int j = 0; j < tile.length; j++) {
+                    //If statement to check if the current location is an outer edge, meaning it is a habitat slot
+                    if (i == 0 || i == tile.length - 1 || j == 0 || j == tile[i].length - 1) {
+                        //Fill with the habitat
+                        this.tile[i][j] = habitats.get(0) + space + ViewColours.RESET;
+                        //Else it is in the center of the tile
                     } else {
-                        this.tile[i][j] = habitat.get(0) + space + ViewColours.RESET;
-                    }
-                } else {
-                    if(animal.size() > 0) {
-                        this.tile[i][j] = animal.get(0).toString();
-                        animal.remove(0);
-                    } else {
-                        this.tile[i][j] = blankSpace;
+                        //If to check that the index is in the first position for wildlife
+                        if (i == 1 && j == 1 && wildlife.size() == 1) {
+                            this.tile[i][j] = wildlife.get(0).toString();
+                        } else {
+                            //Else print blank white space
+                            this.tile[i][j] = whiteSpace;
+                        }
                     }
                 }
             }
         }
+
+        //Constructor for a random tile
+        else if(type == tileTypes.RANDOM) {
+            //Block to make a habitat arraylist and fill it with 2 different habitats
+            habitats = new ArrayList<Habitat>();
+            Habitat hab1 = Habitat.chooseHabitat();
+            Habitat hab2 = Habitat.chooseHabitat();
+            while(hab1 == hab2) {
+                hab2 = Habitat.chooseHabitat();
+            }
+            habitats.add(hab1);
+            habitats.add(hab2);
+            //Block to make a wildlife arraylist and fill it with randomly selected 2 or 3 different wildlife
+            wildlife = new ArrayList<Wildlife>();
+            Random rnd = new Random();
+            Wildlife wild1 = Wildlife.chooseWildlife();
+            Wildlife wild2 = Wildlife.chooseWildlife();
+            while(wild1 == wild2) {
+                wild2 = Wildlife.chooseWildlife();
+            }
+            wildlife.add(wild1);
+            wildlife.add(wild2);
+            //Random if and while loop for if a 3rd wildlife is selected
+            if(rnd.nextInt(3) == 2) {
+                Wildlife wild3 = Wildlife.chooseWildlife();
+                while(wild2 == wild3  || wild1 == wild3) {
+                    wild3 = Wildlife.chooseWildlife();
+                }
+                wildlife.add(wild3);
+            }
+
+            //Double for loop to iterate through each location in the tile array
+            for(int i = 0; i < tile.length; i++) {
+                for(int j = 0; j < tile.length; j++) {
+                    //If to check if its an outer edge of the matrix
+                    if(i == 0 || i == tile.length - 1 || j == 0 || j == tile[i].length - 1) {
+                        //Secondary if to split between the first habitat locations and the second
+                        if(j < 2) {
+                            this.tile[i][j] = habitats.get(0) + space + ViewColours.RESET;
+                        } else {
+                            this.tile[i][j] = habitats.get(1) + space + ViewColours.RESET;
+                        }
+                        //Else it is in the center of the tile
+                    } else {
+                        //Multiple if's to check the wildlife locations and if there are 2 or 3 wildlife
+                        if (i == 1 && j == 1 && wildlife.size() >= 1) {
+                            this.tile[i][j] = wildlife.get(0).toString();
+                        } else if (i == 1 && j == 2 && wildlife.size() >= 2) {
+                            this.tile[i][j] = wildlife.get(1).toString();
+                        } else if (i == 2 && j == 1 && wildlife.size() == 3) {
+                            this.tile[i][j] = wildlife.get(2).toString();
+                        } else {
+                            this.tile[i][j] = whiteSpace;
+                        }
+                    }
+                }
+            }
+
+        }
+
+
+        //Else its an empty tile
+       else {
+           for(int i = 0; i < tile.length; i++) {
+               //Fill tile with space
+               Arrays.fill(tile[i], space);
+           }
+           this.blank = true;
+       }
+    }
+
+    //Rotate function to rotate a tile by 2 slots
+    public void rotate() {
+        String tmp = tile[1][0];
+        String tmp2 = tile[2][0];
+        tile[1][0] = tile[3][0];
+        tile[2][0] = tile[3][1];
+        tile[3][0] = tile[3][2];
+        tile[3][1] = tile[3][3];
+        tile[3][2] = tile[2][3];
+        tile[3][3] = tile[1][3];
+        tile[2][3] = tile[0][3];
+        tile[1][3] = tile[0][2];
+        tile[0][3] = tile[0][1];
+        tile[0][2] = tile[0][0];
+        tile[0][1] = tmp;
+        tile[0][0] = tmp2;
     }
 
 //    Tile(Habitat habitat, Habitat habitat2, ArrayList<Wildlife> animal) {
@@ -69,6 +168,7 @@ public class Tile {
 //        }
 //    }
 
+    //Draw method to draw a whole tile in its own row
     public void draw() {
         String out = "";
         for(int i = 0; i < tile.length; i++) {
@@ -80,6 +180,7 @@ public class Tile {
         }
     }
 
+    //To string method used to draw tiles in-line
     public String toString(int row) {
         String out ="";
         for(int j = 0; j < tile[row].length; j++) {
@@ -88,13 +189,17 @@ public class Tile {
         return out;
     }
 
+    //Method to check if the tile is blank
     public Boolean isBlank() {
         return blank;
     }
 
+    //Method to return the wildlife arraylist
     public ArrayList<Wildlife> getAnimals() {
         return wildlife;
     }
+
+    //Method to return the habitat arraylist
     public ArrayList<Habitat> getHabitats() {
         return habitats;
     }
