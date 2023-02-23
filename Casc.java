7693b561c902e7e70ad33d6417c8d3d1f9c4c1c3
Alexey-B0 <alexey.budnyev@ucdconnect.ie>
2023-02-23 20:24:36 +0000

    Deck empty message should appear now when the deck is empty

--- Casc.java
+++ Casc.java
@@ -69,5 +69,8 @@ public class Casc {
         if(command.isQuit()) {
             view.showQuit();
         }
+        else if(command.DeckisFin()) {
+            view.showDeckEmpty();
+        }
     }
 }
